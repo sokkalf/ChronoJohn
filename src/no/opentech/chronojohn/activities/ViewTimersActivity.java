d15765174657a944b83d86daa30435a84373847e
Christian Lønaas <christian.lonaas@discombobulator.org>
2011-12-28 23:24:25 +0100

    initial commit

---
+++ src/no/opentech/chronojohn/activities/ViewTimersActivity.java
@@ -0,0 +1,15 @@
+package no.opentech.chronojohn.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by: Christian Lønaas
+ * Date: 28.12.11
+ * Time: 23:19
+ */
+public class ViewTimersActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ src/no/opentech/chronojohn/models/ViewTimersModel.java
@@ -0,0 +1,9 @@
+package no.opentech.chronojohn.models;
+
+/**
+ * Created by: Christian Lønaas
+ * Date: 28.12.11
+ * Time: 23:20
+ */
+public class ViewTimersModel {
+}
